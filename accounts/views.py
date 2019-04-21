bca2da50b5f3275f5f7d0fabb64034fa3fd5927a
Seun Odewale <seunodewale.zone5@gmail.com>
2019-04-21 23:14:13 +0100

    add logic to register users

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+    #    Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check passwords
+        if password == password2:
+            # Check username in database
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is taken')
+                    return redirect('register')
+                else:
+                    # Looks good
+                    user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+                    # Login after register
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in!')
+                    user.save()
+                    messages.success(request, 'You are now registered and can login!')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+
     else:
         return render(request, 'accounts/register.html')
     
