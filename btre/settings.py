5261374e4f7bd93cfd3d5fb20020ab52414b66b8
Seun Odewale <seunodewale.zone5@gmail.com>
2019-04-22 16:15:16 +0100

    remove email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -133,13 +133,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',  
 }
 
-# Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'seunmeme19@gmail.com'
-EMAIL_HOST_PASSWORD = 'connectme'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
